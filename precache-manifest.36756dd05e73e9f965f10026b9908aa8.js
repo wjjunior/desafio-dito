aaaeb1c7927ba19526dac63a30c0920dcabdab26
Wagner <wagner.junior30@gmail.com>
2019-02-28 19:08:49 -0300

    Updates

---
+++ precache-manifest.36756dd05e73e9f965f10026b9908aa8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "22792c808fc355157599",
+    "url": "/desafio-dito/static/css/main.6a9abed1.chunk.css"
+  },
+  {
+    "revision": "22792c808fc355157599",
+    "url": "/desafio-dito/static/js/main.22792c80.chunk.js"
+  },
+  {
+    "revision": "184231516c57502243d4",
+    "url": "/desafio-dito/static/js/runtime~main.18423151.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/desafio-dito/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "a8c725dd0559601e74a35235843a0e9b",
+    "url": "/desafio-dito/index.html"
+  }
+];
